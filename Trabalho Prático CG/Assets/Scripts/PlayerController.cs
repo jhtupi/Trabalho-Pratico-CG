5f4c8a29edcf65cb84dd253208e7337a43a038db
Matheus Bissoli <matheusa1011@gmail.com>
2019-06-02 21:57:22 -0300

    algumas modificacoes nos scripts, falta os inimigos para terminar

--- Trabalho Prático CG/Assets/Scripts/PlayerController.cs
+++ Trabalho Prático CG/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Boundary
@@ -19,7 +21,26 @@ public class PlayerController : MonoBehaviour
     public float fireRate;
 
     private float nextFire;
-  
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+    }
+
+
     void Update()
     {        
         if (Input.GetButton("Fire1") && Time.time > nextFire)
@@ -28,6 +49,14 @@ public class PlayerController : MonoBehaviour
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             //shotSpawn.localScale(0.1f, 0.1f, 0.1f);
             //GetComponent<AudioSource>().Play();
+        }
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
         }
     }
 
@@ -57,6 +86,29 @@ public class PlayerController : MonoBehaviour
             GetComponent<Rigidbody>().rotation = Quaternion.Euler(GetComponent<Rigidbody>().velocity.y * -tilt, 0.0f, 0.0f);
         }
 
-        
+        if (gameOver)
+        {
+            restartText.text = "Press 'R' for Restart";
+            restart = true;
+        } //conciliar com os inimigos
+
+
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
     }
 }
