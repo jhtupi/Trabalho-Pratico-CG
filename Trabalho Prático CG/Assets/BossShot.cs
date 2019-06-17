d279a84c591057400d7e47621d2e309c603064cd
Jhtupi <jhtupi@gmail.com>
2019-06-17 11:10:57 -0300

    Boss shot dá dano no player

---
+++ Trabalho Prático CG/Assets/BossShot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class BossShot : MonoBehaviour
+{
+    public float shotSpeed;
+    public int shotDamage;
+    public GameObject playerExplosion, self;
+
+    private GameController gameController;
+    private Slider vida;
+
+    void Start()
+    {
+        if (GameObject.Find("CanvasVida"))
+        {
+            vida = (Slider)FindObjectOfType(typeof(Slider));
+        }
+
+        gameController = GameObject.Find("Game Controller").GetComponent<GameController>();
+
+        GetComponent<Rigidbody>().velocity = transform.forward * shotSpeed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.tag == "Player")
+        {
+            if (vida.value > 0)
+            {
+                vida.value -= shotDamage;
+                Destroy(self);
+                return;
+            }
+            else
+            {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+                Destroy(other.gameObject);
+                gameController.GameOver();
+            }
+
+        }
+        Destroy(self);
+
+    }
+}
+
